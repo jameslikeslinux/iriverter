a0f1a88f1508d4c4be0227c99f032472483d9d9d
James Lee <jlee@thestaticvoid.com>
2006-12-13 22:59:03 +0000

    Copyright notice on all source files

--- src/org/thestaticvoid/iriverter/Converter.java
+++ src/org/thestaticvoid/iriverter/Converter.java
@@ -1,3 +1,24 @@
+/*
+ * Converter.java
+ * Copyright (C) 2005-2007 James Lee
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ * 
+ * $Id$
+ */
 package org.thestaticvoid.iriverter;
 
 import java.io.*;
@@ -234,10 +255,10 @@ public class Converter extends Thread {
 		commandList.add("-vf-add");
 		commandList.add("harddup");
 		
-		if (ConverterOptions.getVolumeFilter().equals("volnorm")) {
+		if (ConverterOptions.getVolumeFilter().equals(VolumeFilter.VOLNORM)) {
 			commandList.add("-af");
 			commandList.add("volnorm");
-		} else if (ConverterOptions.getVolumeFilter().equals("volume")) {
+		} else if (ConverterOptions.getVolumeFilter().equals(VolumeFilter.VOLUME)) {
 			commandList.add("-af");
 			commandList.add("volume=" + ConverterOptions.getGain());
 		}
--- src/org/thestaticvoid/iriverter/VolumeDialog.java
+++ src/org/thestaticvoid/iriverter/VolumeDialog.java
@@ -1,3 +1,24 @@
+/*
+ * VolumeDialog.java
+ * Copyright (C) 2005-2007 James Lee
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ * 
+ * $Id$
+ */
 package org.thestaticvoid.iriverter;
 
 import org.eclipse.swt.*;
@@ -40,19 +61,19 @@ public class VolumeDialog extends Dialog implements SelectionListener {
 		
 		filterNone = new Button(filterGroup, SWT.RADIO);
 		filterNone.setText("Apply no volume filter");
-		filterNone.setSelection(ConverterOptions.getVolumeFilter().equals("none"));
+		filterNone.setSelection(ConverterOptions.getVolumeFilter().equals(VolumeFilter.NONE));
 		GridData gridData = new GridData();
 		gridData.horizontalSpan = 3;
 		filterNone.setLayoutData(gridData);
 		
 		filterVolnorm = new Button(filterGroup, SWT.RADIO);
 		filterVolnorm.setText("Normalize the volume");
-		filterVolnorm.setSelection(ConverterOptions.getVolumeFilter().equals("volnorm"));
+		filterVolnorm.setSelection(ConverterOptions.getVolumeFilter().equals(VolumeFilter.VOLNORM));
 		filterVolnorm.setLayoutData(gridData);
 		
 		filterVolume = new Button(filterGroup, SWT.RADIO);
 		filterVolume.setText("Manually specify gain:");
-		filterVolume.setSelection(ConverterOptions.getVolumeFilter().equals("volume"));
+		filterVolume.setSelection(ConverterOptions.getVolumeFilter().equals(VolumeFilter.VOLUME));
 		
 		gainText = new Text(filterGroup, SWT.BORDER);
 		// forces width large width
@@ -101,16 +122,16 @@ public class VolumeDialog extends Dialog implements SelectionListener {
 		
 		if (e.getSource() == ok) {
 			if (filterNone.getSelection())
-				ConverterOptions.setVolumeFilter("none");
+				ConverterOptions.setVolumeFilter(VolumeFilter.NONE);
 			else if (filterVolnorm.getSelection())
-				ConverterOptions.setVolumeFilter("volnorm");
+				ConverterOptions.setVolumeFilter(VolumeFilter.VOLNORM);
 			else {
 				try {
 					double volume = Double.parseDouble(gainText.getText());
 					if (volume < -200.0 || volume > 60.0)
 						throw new Exception();
 					
-					ConverterOptions.setVolumeFilter("volume");
+					ConverterOptions.setVolumeFilter(VolumeFilter.VOLUME);
 					ConverterOptions.setGain(volume);
 				} catch (Exception exception) {
 					MessageBox messageBox = new MessageBox(shell, SWT.ICON_ERROR);
